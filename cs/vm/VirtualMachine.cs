602d0d0d726a54379749b20723e8a8150bae6986
Ted Neward <ted@tedneward.com>
2022-06-14 19:39:30 -0700

    Making sure Step 0 reads correctly

--- cs/vm/VirtualMachine.cs
+++ cs/vm/VirtualMachine.cs
@@ -54,6 +54,42 @@ public enum Bytecode
 
 public class VirtualMachine
 {
+    // Tracing
+    //
+    bool trace = false;
+    private void Trace(string message)
+    {
+        if (trace)
+            Console.WriteLine("TRACE: {0}", message);
+    }
+
+    // Diagnostics
+    //
+    private void Dump()
+    {
+        Console.WriteLine("SimpleVM - DUMP");
+        Console.WriteLine("===============");
+        // Uncomment when you implement Execute(Bytecode[])
+        /*
+        Console.WriteLine("IP: {0} / Trace: {1}", IP, trace);
+        Console.WriteLine("Working stack (SP {0}): {1}", SP, String.Join(", ", Stack));
+        */
+        // Uncomment when you implement global values
+        /*
+        Console.WriteLine("Globals: {0}", Globals);
+        */
+        // Uncomment when you implement CallFrames (procedures)
+        /*
+        Console.WriteLine("Call stack: ");
+        for (int f = Frames.Count - 1; f > -1; f--) {
+            CallFrame cf = Frames[f];
+            Console.WriteLine("  Call Frame {0}:", f);
+            Console.WriteLine("  +- ReturnAddr: {0}", cf.ReturnAddress);
+            Console.WriteLine("  +- Locals: {0}", cf.Locals);
+        }
+        */
+    }
+
     public void Execute(Bytecode opcode, params int operands)
     {
 
--- java/vm/src/main/java/simplevm/vm/VirtualMachine.java
+++ java/vm/src/main/java/simplevm/vm/VirtualMachine.java
@@ -28,9 +28,10 @@ public class VirtualMachine {
     private void dump() {
         System.out.println("SimpleVM DUMP");
         System.out.println("=============");
+        /*
         System.out.println("IP: " + ip);
-        System.out.println("Globals: " + Arrays.toString(globals));
         System.out.println("Working stack (SP " + sp + "): " + Arrays.toString(Arrays.copyOfRange(stack, 0, sp+1)));
+        System.out.println("Globals: " + Arrays.toString(globals));
         System.out.println("Call stack: ");
         for (int f = frames.size(); f != 0; f--) {
             CallFrame cf = frames.get(f - 1);
@@ -38,6 +39,7 @@ public class VirtualMachine {
             System.out.println("  +-- Return Address: " + cf.returnAddress);
             System.out.println("  +-- Locals: " + Arrays.toString(cf.locals));
         }
+        */
     }
 
     public void execute(int opcode, int... operands) {
